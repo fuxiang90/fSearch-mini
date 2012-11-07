3cd4571d8ed29b80a7aaed50f43f0b34d7328066
fuxiang90 <fuxiang90@gmail.com>
2012-11-07 20:05:12 +0800

    合并索引

--- pydemo/findex.py
+++ pydemo/findex.py
@@ -62,10 +62,92 @@ class findex(object):
                 s = s+str(termid) +'###'
             fout.write(s+'\n')
         fout.close()   
-
+    #===========================================================================
+    # 合并索引  下次要把各自的依赖关系理清才行
+    #===========================================================================
+    def MergeIndex(self):
+        self.fhashkeyToid = finverted.loadUrlfile(fstd.rootpat+'file/url')
+        
+        fp = open(fstd.rootpat+'file/termid','r')
+        for each in fp:
+            pos1 = each.find('###')
+            termid = int(each[:pos1])
+            self.index[termid] = set()
+            s = each[pos1 + 3:]
+            pos2 = s.find('###')
+            while pos2 != -1 :
+                docid = int(s[:pos2])
+                self.index[termid].add(docid)
+                s = s[pos2+3:]
+                pos2 = s.find('###')
+            
+        fp.close()
+        
+        
+        #对新的文件进行分词
+        os.chdir(fstd.rootpat+'file')
+        fm = fmmseg.fmmseg()
+        fm.loadTermfile()
+        furl = open(fstd.rootpat+'file/newurl','r')
+        for url in furl:
+            url = url[:url.find('\n')]
+            fm.segmentAFile(url+'.text')
+            self.fhashkeyToid[url] = docid
+ 
+        
+        fm.mergeTermJieba()
+        
+        furl.close()
+        #进行索引
+        furl = open(fstd.rootpat+'file/newurl','r')
+        
+        for filename in furl:
+            
+            filename = filename[:filename.find('\n')]
+            filename = filename+'.tmp'
+            if True:
+                print filename
+                fp = open(filename,'r')
+                hashkey = filename
+                if hashkey not in self.fhashkeyToid:
+                    continue
+                docid = self.fhashkeyToid[hashkey]
+                for each in fp:
+                    if each == '\n':
+                        continue
+                    s = each[:each.find('\n')]
+                    while s != '':
+                        pos = s.find('###')
+                        if pos == -1:
+                            break
+                        s1 = s[:pos]
+                        if s1 == '':
+                            break
+                        if s1 not in fm.termdict:
+                            s = s[pos+3:]
+                            continue
+                        id = fm.termdict[s1]
+                        if id not in self.index.keys():
+                                self.index[id] = set()
+                        self.index[id].add(docid)
+                        s = s[pos+3:]
+                    
+                fp.close()
+        
+        print self.index[2] 
+        print '索引建好了'
+        fout = open(fstd.rootpat+'file/termid','w')
+        for termids in self.index.keys():
+            s = str(termids)+'###'
+            for termid in self.index[termids]:
+                s = s+str(termid) +'###'
+            fout.write(s+'\n')
+        fout.close()   
+        
 if __name__ == '__main__':
     f = findex()
-    f.creatIndex()
+#    f.creatIndex()
+    f.MergeIndex()
     print "done it"
         
         
